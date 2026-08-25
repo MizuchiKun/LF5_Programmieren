3de93ea662152d6b5600a6879ba83ad73a94f07e
MizuchiKun <til@jungbluths.de>
2026-08-25 11:04:49 +0200

    Finished exercise 1: Benutzer.

---
+++ src/aufgabenMitEingabe/Benutzer.java
@@ -0,0 +1,19 @@
+package aufgabenMitEingabe;
+
+import java.io.BufferedReader;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Benutzer {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Wie ist dein Vorname?? ");
+        String firstName = reader.readLine().trim();
+        System.out.print("Wie ist dein Nachname?? ");
+        String lastName = reader.readLine().trim();
+
+        System.out.printf("Moin, %s %s!\n", firstName, lastName);
+    }
+}
