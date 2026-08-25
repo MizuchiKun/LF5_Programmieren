9d83786848c3e7bed20d4931667c37d584f5e5f3
MizuchiKun <til@jungbluths.de>
2026-08-25 11:59:27 +0200

    Finished exercise 4: Fahrenheit.

---
+++ src/aufgabenMitEingabe/Fahrenheit.java
@@ -0,0 +1,20 @@
+package aufgabenMitEingabe;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Fahrenheit {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Temperatur in Grad Celsius? ");
+        float tempCelsius = Float.parseFloat(reader.readLine().trim());
+        float tempFahrenheit = (tempCelsius * 9/5f) + 32f;
+
+        if (tempCelsius <= 40f)
+            System.out.printf("%.2f °C sind %.2f °F.\n", tempCelsius, tempFahrenheit);
+        else
+            System.out.println("You call that hot??");
+    }
+}
