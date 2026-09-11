067b878b77fc3fbbc46aaf422ce59b9bd216cd24
MizuchiKun <til@jungbluths.de>
2026-09-11 20:57:38 +0200

    Catching various criteria testing exceptions.
    - Also modified docstrings.
    - And changed file to follow Java conventions (hopefully).

--- src/utils/UserInput.java
+++ src/utils/UserInput.java
@@ -6,74 +6,67 @@ import java.io.InputStreamReader;
 import java.util.function.Predicate;
 
 public class UserInput {
-    private static BufferedReader reader;
+    private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
 
-
-    // How to write function for e.g. ReadString, ReadInt, etc. with a predicate parameter to check if entered
+    // How to write function for e.g. readString, ReadInt, etc. with a predicate parameter to check if entered
     // value meets criteria, while accounting for possible exceptions?
     // There could be e.g. NumberFormatExceptions in the predicate when trying to parse numbers.
+    // Maybe just catch any Exception and print an error message and/or repeat loop?
+    // Though it seems I'll likely only encounter NumberFormatExceptions, when parsing numbers,
+    // which I'll just do for the caller in to-be-done methods in this class, so . . .
 
 
-    /** Reads a String from the user via. the console.
+    /** Reads a String from the user via. the console.<br/>
+     *  Input will be trimmed, and only be accepted, once it meets the given criteria.
      *
      * @param prompt The input prompt message to show the user.
      * @param criteria The criteria for the entered user input.
      * @param errorMessage A message to be displayed to the user if their input did not meet the criteria.
      * @return The user input String.
      */
-    public static String ReadString(String prompt, Predicate<String> criteria, String errorMessage)
-    {
-        initialise();
-
+    public static String readString(String prompt,
+                                    Predicate<String> criteria,
+                                    String errorMessage) {
         // input should have an acceptable value after the loop but the error checker didn't think so, hence the null initialisation.
         // Better solution to this issue?
         String input = null;
         boolean inputMeetsCriteria = false;
-        do
-        {
-            try
-            {
+        do {
+            try {
                 System.out.print(prompt);
-                input = reader.readLine().trim();
+                input = READER.readLine().trim();
 
                 inputMeetsCriteria = criteria.test(input);
-                if (!inputMeetsCriteria)
-                {
+                if (!inputMeetsCriteria) {
                     System.out.print(errorMessage);
                 }
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 System.out.println("There was an IOException reading your input. Try again.");
+            } catch (Exception e) {
+                // Must mean something went wrong when testing criteria.
+                System.out.printf("There's likely something wrong with the code (error: %s), but try again.\n",
+                                  e.getClass().getSimpleName());
             }
         } while (!inputMeetsCriteria);
 
         return input;
     }
 
-    /** Reads a String from the user via. the console.
+    /** Reads a String from the user via. the console.<br/>
+     *  Input will be trimmed.
      *
      * @param prompt The input prompt message to show the user.
      * @return The user input String.
      */
-    public static String ReadString (String prompt)
-    {
+    public static String readString(String prompt) {
         final Predicate<String> NO_CRITERIA = (String) -> true;
-        return ReadString(prompt, NO_CRITERIA, "");
+        return readString(prompt, NO_CRITERIA, "");
     }
 
-    /** Initialises the BufferedReader. **/
-    private static void initialise ()
-    {
-        if (reader == null)
-        {
-            reader = new BufferedReader(new InputStreamReader(System.in));
-        }
-    }
-
+    /** Just does stuff for testing. **/
     public static void main(String[] args) {
-        ReadString("Gimme a String: ",
+        readString("Gimme a String: ",
                    (String input) -> input.length() >= 5,
-       "String must be at least 5 characters long.\n");
+                   "String must be at least 5 characters long.\n");
     }
 }
