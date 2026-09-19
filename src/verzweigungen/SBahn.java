c422010eaab66840feb3d4f75ca6bdf731aea355
MizuchiKun <til@jungbluths.de>
2026-09-19 21:37:33 +0200

    Refactored SBahn a bit.

--- src/verzweigungen/SBahn.java
+++ src/verzweigungen/SBahn.java
@@ -7,20 +7,23 @@ public class SBahn {
         int endLine = end / 10;
         int endStation = end % 10;
 
-        final int NUMBER_OF_LINES = 5;
-        final int RING_LINE_STATION = 3;
+        final int LINE_COUNT = 5;
+        final int RING_STATION = 3;
         final int FINAL_STATION = 6;
         double priceInEuro = 0;
-        boolean areLineNeighbours = (startStation + endStation == 1)
+
+        boolean areNeighboursInCentre = startStation + endStation == 1;
+        boolean areLineNeighbours = areNeighboursInCentre
                                     || (startLine == endLine && Math.abs(startStation - endStation) == 1);
-        boolean areRingNeighbours = (startStation == RING_LINE_STATION && endStation == RING_LINE_STATION)
+        boolean areRingNeighbours = (startStation == RING_STATION && endStation == RING_STATION)
                                     && (Math.abs(startLine - endLine) == 1
-                                        || startLine + endLine == NUMBER_OF_LINES + 1);
+                                        || startLine + endLine == LINE_COUNT + 1);
         boolean areNeighbouringStations = areLineNeighbours || areRingNeighbours;
+
         if (areNeighbouringStations) {
             priceInEuro = 1;
         } else {
-            priceInEuro = 2;  // 2 € base price.
+            priceInEuro = 2;
 
             boolean startsAtEndOfLine = (startStation == 0 || startStation == FINAL_STATION);
             boolean endsAtEndOfLine = (endStation == 0 || endStation == FINAL_STATION);
@@ -31,8 +34,8 @@ public class SBahn {
                 priceInEuro += 1;
             }
 
-            boolean startsInZone1 = startStation <= RING_LINE_STATION;
-            boolean endsInZone1 = endStation <= RING_LINE_STATION;
+            boolean startsInZone1 = startStation <= RING_STATION;
+            boolean endsInZone1 = endStation <= RING_STATION;
             boolean crossedBoundaryExplicitly = (startsInZone1 ^ endsInZone1);
             boolean crossedBoundaryImplicitly = (!startsInZone1 && !endsInZone1)
                                                 && startLine != endLine;
