590b67cbdc1d2faa105a64aff78973ff4341f11f
MizuchiKun <til@jungbluths.de>
2026-09-19 19:21:12 +0200

    Finished AB+ exercise: DatumValidierer.

--- src/verzweigungen/DatumValidierer.java
+++ src/verzweigungen/DatumValidierer.java
@@ -1,6 +1,16 @@
 package verzweigungen;
 
 public class DatumValidierer {
+    private static final byte JANUARY = 1, FEBRUARY = 2, MARCH = 3,
+                              APRIL = 4, MAY = 5, JUNE = 6,
+                              JULY = 7, AUGUST = 8, SEPTEMBER = 9,
+                              OCTOBER = 10, NOVEMBER = 11, DECEMBER = 12;
+
+    private static boolean isLeapYear(int year) {
+        return year % 400 == 0
+                || (year % 4 == 0 && year % 100 != 0);
+    }
+
     /** Checks if the given date is a valid date.
      *
      * @param day   The day of the date.
@@ -9,10 +19,17 @@ public class DatumValidierer {
      * @return  True if the date is valid, false otherwise.
      */
     public static boolean validiereDatum(int day, int month, int year) {
-        return false;
-    }
+        boolean isALeapYear = isLeapYear(year);
+        boolean isValidMonth = 1 <= month && month <= 12;
+        if (!isValidMonth) {
+            return false;
+        }
 
-    public static void main(String[] args) {
-        DatumValidiererTest.data_GivenDate_DateValidOrNot();
+        return switch (month) {
+            case FEBRUARY -> isALeapYear ? 1 <= day && day <= 29
+                                         : 1 <= day && day <= 28;
+            case JANUARY, MARCH, MAY, JULY, AUGUST, OCTOBER, DECEMBER -> 1 <= day && day <= 31;
+            default -> 1 <= day && day <= 30;
+        };
     }
 }
